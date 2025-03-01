99c2c8ac8a5417104465b6eae75aa66e2f3d0594
omer <omerde682@gmail.com>
2025-03-01 22:48:04 +0200

    output file to desktop

--- coordinator/app/logic/output_writer.py
+++ coordinator/app/logic/output_writer.py
@@ -2,17 +2,16 @@ import csv
 import os
 from typing import List
 
+OUTPUT_PATH = "/app/output"
+
 
 def output_writer(output_rows: List[List[str]], original_file_name: str) -> str:
-
     base_name = os.path.splitext(original_file_name)[0]
-    output_file = f"cracked_{base_name}.csv"
-
+    output_file = os.path.join(OUTPUT_PATH, f"cracked_{base_name}.csv")
     with open(output_file, mode="w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(["hash", "cracked_password"])
         writer.writerows(output_rows)
-
     output_path = os.path.abspath(output_file)
     print(f"Output file written to {output_path}")
     return output_path
